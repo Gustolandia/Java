677d5a8e0692079838b900130cbd0d5147e63daa
Gustavo Pedro Ricou <63708303+Gustolandia@users.noreply.github.com>
2021-12-08 18:52:12 +0000

    1st exercise practical 1

---
+++ Practical 1/Hello_world.java
@@ -0,0 +1,13 @@
+// Practical 1
+// Author: Gustavo Ricou
+// Date: 08/12/21
+// Program to write Hello World to screen
+
+//Question 1
+
+public class Hello_world{
+   public static void main(String [] args){
+      System.out.print("Hello World \n");
+      System.out.print("This is our first program");
+      }
+   }     
